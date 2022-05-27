fe761571258da676417bae2adee72fc12d606f1f
RyoIwahara <riwahara@mini.local>
2022-05-27 23:39:52 +0900

    github actions test

---
+++ src/main/kotlin/com/muscletracking/mtapi/controller/UserRestController.kt
@@ -0,0 +1,12 @@
+package com.muscletracking.mtapi.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class UserRestController {
+    @GetMapping("/demo")
+    fun demo():String {
+        return "Hello World!"
+    }
+}
