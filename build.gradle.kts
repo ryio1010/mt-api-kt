4994a6bdf0685b4f57b3ad739f5f9af33795772c
RyoIwahara <riwahara@mini.local>
2022-05-29 22:56:29 +0900

    google app engineへのデプロイ設定追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+	repositories {
+		mavenCentral()
+	}
+	dependencies {
+		classpath("org.springframework.boot:spring-boot-gradle-plugin:2.2.0.RELEASE")
+		classpath("com.google.cloud.tools:appengine-gradle-plugin:2.2.0'")
+	}
+}
+
 plugins {
 	id("org.springframework.boot") version "2.7.0"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	id("com.google.cloud.tools.appengine") version "2.2.0"
 }
 
 group = "com.muscletracking"
@@ -26,6 +37,7 @@ dependencies {
 	implementation ("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.google.appengine:appengine-api-1.0-sdk:+")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
@@ -35,6 +47,19 @@ dependencies {
 	// junit5
 	testImplementation("org.junit.jupiter:junit-jupiter-api")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+appengine {
+	// デプロイ時の設定
+	// GCLOUD_CONFIG を指定しておくと
+	// gcloud config で設定しているプロジェクト情報がセットされる
+	deploy {
+		// デプロイ先の Google Cloud Project ID
+		projectId = "individual-development"
+		// デプロイによって反映される Web アプリのバージョン
+		// 指定しなければ新しく生成される
+		version = "GCLOUD_CONFIG"
+	}
 }
 
 tasks.withType<KotlinCompile> {
