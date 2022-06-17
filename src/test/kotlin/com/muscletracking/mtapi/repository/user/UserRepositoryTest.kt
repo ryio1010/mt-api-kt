31fc519c67c9615cb732e5d15429b33d5115fed7
RyoIwahara <ryo.iwahara1010@outlook.jp>
2022-06-17 22:59:01 +0900

    テストの追加

--- src/test/kotlin/com/muscletracking/mtapi/repository/user/UserRepositoryTest.kt
+++ src/test/kotlin/com/muscletracking/mtapi/repository/user/UserRepositoryTest.kt
@@ -1,5 +1,13 @@
 package com.muscletracking.mtapi.repository.user
 
+import com.muscletracking.mtapi.dao.user.UserDao
+import com.muscletracking.mtapi.entity.user.User
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 
@@ -8,8 +16,12 @@ import org.junit.jupiter.api.BeforeEach
 
 internal class UserRepositoryTest {
 
+    @InjectMockKs
+    private var userRepository: UserRepository = UserRepository()
+
     @BeforeEach
     fun setUp() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
     }
 
     @AfterEach
@@ -18,5 +30,13 @@ internal class UserRepositoryTest {
 
     @Test
     fun getUserById() {
+        val expected = User(id = "ryio1010", userName = "ryo", password = "ryio1010")
+
+        val userDao = mockk<UserDao>()
+        every { userDao.selectById(any()) } returns expected
+
+        val actual = userRepository.getUserById("001")
+
+        verify { userRepository.getUserById("001") }
     }
 }
